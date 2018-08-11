196d4570a3e81130057c0cc7e39c0eb38e71c59b
Luka <luka.maske@gmail.com>
2018-08-11 12:09:45 +0200

    Added audio manager.

---
+++ GooShooter/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	private static AudioManager instance;
+
+	[SerializeField] private AudioSource music = null;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+	private void Awake()
+	{
+		instance = this;
+	}
+}
