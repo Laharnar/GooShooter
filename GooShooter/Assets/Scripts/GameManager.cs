c0eba347e43b5abb988038f837ebf83d7a8a4cd6
Luka <luka.maske@gmail.com>
2018-08-11 12:05:14 +0200

    Updated gamenanager.

--- GooShooter/Assets/Scripts/GameManager.cs
+++ GooShooter/Assets/Scripts/GameManager.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance;
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("Test");
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+	private void Awake()
+	{
+		instance = this;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   
 }
