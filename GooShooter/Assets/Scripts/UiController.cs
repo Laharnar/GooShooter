104e49b1a9473f7a0da8b9fa665bf229cd4de529
Luka <luka.maske@gmail.com>
2018-08-11 12:36:12 +0200

    Added ui components.

---
+++ GooShooter/Assets/Scripts/UiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    [SerializeField] private Camera canvasCamera = null;
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+}
