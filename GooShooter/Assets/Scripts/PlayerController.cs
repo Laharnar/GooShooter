d5549547c8e51e18cd1871b80c8fa783b98668a8
Luka <luka.maske@gmail.com>
2018-08-12 10:41:54 +0200

    Added player and blocks.

--- GooShooter/Assets/Scripts/PlayerController.cs
+++ GooShooter/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,70 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Camera cam;
+    public float movementSpeed = 2;
+    public float cameraFollowSpeed = 2;
+    private Vector3 cameraOffset;
+    public Animator playerAnimator;
 
+    private void Start()
+    {
+        cameraOffset = cam.transform.position;
+        cameraOffset.y = 0;
+    }
     private void Update()
     {
+        HandlePlayerMovement();
         RotatePlayerTowardsMouse();
+        CameraFollowPlayer();
+        UpdateShooting();
+    }
+
+    private void UpdateShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerAnimator.SetTrigger("Shoot");
+        }
+    }
+
+    private void CameraFollowPlayer()
+    {
+        Vector3 movementDirecton = transform.position - cam.transform.position + cameraOffset;
+        movementDirecton.y = 0;
+        cam.transform.position += movementDirecton * Time.deltaTime * cameraFollowSpeed;
+    }
+
+    private void HandlePlayerMovement()
+    {
+        bool moved = false;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+            moved = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * Time.deltaTime * movementSpeed;
+            moved = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * movementSpeed;
+            moved = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+            moved = true;
+        }
+        if (moved)
+        {
+            playerAnimator.SetInteger("Run", 1);
+        }
+        else
+        {
+            playerAnimator.SetInteger("Run", 0);
+        }
     }
 
     private void RotatePlayerTowardsMouse()
