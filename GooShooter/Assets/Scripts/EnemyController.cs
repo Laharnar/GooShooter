17aa7e7c0c1a7abda8e446e549dcdf9d2a4a8aae
Luka <luka.maske@gmail.com>
2018-08-12 15:25:37 +0200

    Updated player and ooze.

--- GooShooter/Assets/Scripts/EnemyController.cs
+++ GooShooter/Assets/Scripts/EnemyController.cs
@@ -6,45 +6,54 @@ using UnityEngine.AI;
 /// <summary>
 /// Follow player, on 
 /// </summary>
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
 
     public int health = 5;
 
     public Animator anim;
     public NavMeshAgent nav;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         tag = "Enemy";
+    }
 
-        anim.Play("Walk");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         nav.destination = GameManager.Instance.player.transform.position;
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.transform == GameManager.Instance.player.transform) {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform == GameManager.Instance.player.transform)
+        {
             Death();
         }
     }
 
-    public void Damage(int dmg) {
+    public void Damage(int dmg)
+    {
         health -= dmg;
-        if (health <=0) {
+        if (health <= 0)
+        {
             Death();
         }
         //SpawnOoze(transform.position);
     }
 
-    private void Death() {
+    private void Death()
+    {
         // TODO: spawn effects/death animation
-        Destroy(gameObject);
+        anim.SetTrigger("Death");
+        Destroy(gameObject, 1);
+        
     }
 
-    public void SpawnOoze() {
+    public void SpawnOoze()
+    {
         //
     }
 }
